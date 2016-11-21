6b9090bbdd5f83545ad9499c56e3ecbf170bb031
Kevin Mach <mr.kevinmach@gmail.com>
2016-11-21 15:00:28 -0800

    Initial Commit

---
+++ AutomatedTellerMachine/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // GET /home/index
+            return View();
+        }
+
+        // GET /home/about
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //Foo to be an ailias to the about page. 
+        public ActionResult Foo()
+        {
+            return View("About"); 
+        }
+    }
+}
